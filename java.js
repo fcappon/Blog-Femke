30ca3bddbc0b1fa4a6b633e1ef8183176ccee339
fcappon <118693275+fcappon@users.noreply.github.com>
2023-04-09 10:12:50 +0200

    digital clocks
    Added digital clocks of london-ams-and suzhou time zones

--- java.js
+++ java.js
@@ -2,3 +2,25 @@ const languageSelect = document.getElementById('language-select');
 languageSelect.addEventListener('change', function() {
   document.body.setAttribute('data-language', this.value);
 });
+
+/* DIGITAL CLOCKS */
+
+const locations = document.querySelectorAll("section.times div")
+
+const updateTimes = function(){
+    locations.forEach(location => {
+        const output = location.querySelector("output")
+        const timezone = location.getAttribute("data-timezone")
+        
+        
+        const now = luxon.DateTime.now().setZone(timezone)
+    
+        output.innerHTML = now.toFormat("HH:mm:ss")
+    })
+}
+
+updateTimes()
+
+setInterval(function(){
+    updateTimes()
+},1000)
